dc9f0f29262d549a28fd9822d3d678c19855d055
Barovbe <danielbarovbe@gmail.com>
2017-09-08 00:05:35 +0100

    Removed silly text by the wasteman joseph

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 		<title>Save The Big Day - Home</title>
 	</head>
 	<body>
-		ergbdvssdvergvfdgvfdsgsggssbgsfdg
 		<?php include "gnavbar.php" ?>
 		<div class = "logoSection"><img src="img/stbdLogo.png" alt="Logo" style="width:320px"></div>
 		<div class = "searchSection">
