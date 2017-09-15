87f57cebc84c2ea99ff63ec65dd3fdef85175d48
Barovbe <danielbarovbe@gmail.com>
2017-09-15 22:18:18 +0100

    Changed tag input from enter to spacebar

--- javascript/tagList.js
+++ javascript/tagList.js
@@ -8,7 +8,7 @@ var tagNo=0;
 
 tagInput.addEventListener("keypress", function (e) {
 	var key = e.keyCode;
-	if ((key === 13)&&(tagInput.value!=="")) { // 13 is enter
+	if ((key === 32)&&(tagInput.value!=="")) { // 13 is enter
 		var isDuplicate = false;
  		for(i=0;i<tags.length;i++){
  			if(tags[i] == tagInput.value){ //Checks if a tag has the same value as the input
