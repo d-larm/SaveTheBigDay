49e5b5bcfb9d551f76520ad3d531ed0b59bdb877
Daniel <danielbarovbe@gmail.com>
2017-09-22 18:12:25 +0100

    Integrating backend search functions

--- venue.php
+++ venue.php
@@ -5,8 +5,8 @@
 		<?php include "scripts.php" ?>
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/style2.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<title>Save The Big Day - Home</title>
+		<link rel="stylesheet" href="css/gridCollapsible.css">
+		<title>Save The Big Day - Search</title>
 	</head>
 	<body style="background-color:white" ng-app="app">
 		<?php include "gnavbar2.php" ?>
@@ -18,7 +18,8 @@
 			<div class="row">
 				<div class="col-3">
 					<h1 align=center>Tags</h1>
-					<h2><?php
+					<h2> <!--Prints out the tags inputted in the search !-->
+						<?php
 							if (isset($_GET['tags'])) {
 						        $tags = explode("-",$_GET["tags"]);
 						        foreach($tags as $tag)
@@ -26,12 +27,45 @@
 						    }else{
 						        echo "Tags not set";
 						    }
-
-
-
-						?></h2>
+						?>
+					</h2>
 				</div>
+				<div class=vendorPageList>
+					<div class="col-3">
+						<div class="vendorElement" style="padding:10px;border:1px solid grey;height:auto;min-height:400px;">
+							<img class="vendorImage" src="/img/fbLogoDark.png" style="width:100%;height:auto;">
+							<h2 class="vendorTitle">This is a vendor element</h2>
+							<p class="vendorText">This is a well thought message to the people of the world about how moist joseph is</p>
+						</div>
+					</div>
+				</div>
+				
+
+			</div>
 		</div>
 	</body>
+	<script>
+		data =<?php
+			$tags = $_GET["tags"];
+			$location = $_GET["postcode"];
+
+			database = new Database();
+			$data = array();
+			foreach($tags as $tag){
+				$query = $database->query("SELECT * FROM VendorPages WHERE tags LIKE '%{$tag}%'");
+				while(($page = mysqli_fetch_assoc($query))){
+					array_push($data,$page); //Pushes page elements that have matching tag to array
+			    }
+			}	
+		    echo json_encode(array_unique($data)); //Returrns an array where all pages are unique
+		?>
+		for(i=0;i<data.length;i++){
+			$(".vendorPageList").append("<div class=col-3><div class=vendorElement style='padding:10px;border:1px solid grey;height:auto;min-height:400px;'>");
+			$(".vendorPageList").append("<img class='vendorImage' src='/img/"+data[i]["VendorID"]+"/v/v(0).png' style='width:100%;height:auto;'>");
+			$(".vendorPageList").append("<h2 class=vendorTitle>"+data[i]["Name"]+"</h2>");
+			$(".vendorPageList").append("<p class=vendorText>"+data[i]["Content"].substring(0,25)+"</p>");
+		}
+		
+	</script>
 	
 </html>
